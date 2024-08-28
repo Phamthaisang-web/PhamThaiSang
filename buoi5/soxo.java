2a5806fa4cdbbecca2813ae55eab75074c5fbb24
Sang Fan <phamthaisang1710@gmail.com>
2024-08-28 09:28:05 +0700

    Update soxo.java

--- buoi5/soxo.java
+++ buoi5/soxo.java
@@ -13,9 +13,8 @@ public class soxo {
         Random rd=new Random();
         System.out.println("Choi so xo.");
         System.out.println("KQ so xo. ");
-        for(int i=1;i<=6;i++){
             int n = rd.nextInt(100);
             System.out.print(n +" ");
-        }
+        
     }
 }
