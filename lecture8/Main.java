d3c778ede0f7a2cca10da9e6ecb76d5dac7a55b4
Sang Fan <phamthaisang1710@gmail.com>
2024-09-05 18:04:18 +0700

    Add files via upload

---
+++ lecture8/Main.java
@@ -0,0 +1,14 @@
+
+package lecture8;
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+        Student hs1 = new Student(1," sang");
+        Student hs2 = new Student(2," pham");
+        hs1.displayinformation();
+        hs2.displayinformation();
+    }
+    
+}
