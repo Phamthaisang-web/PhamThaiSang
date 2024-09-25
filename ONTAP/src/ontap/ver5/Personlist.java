d750e77051748aa30ef5358846f00b7340801eec
Sang Fan <phamthaisang1710@gmail.com>
2024-09-25 09:30:58 +0700

    Add files via upload

---
+++ ONTAP/src/ontap/ver5/Personlist.java
@@ -0,0 +1,89 @@
+package ver5;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Personlist {
+
+    Scanner sc = new Scanner(System.in);
+    ArrayList<Person> peronlist = new ArrayList<>();
+
+    void addStudent(Student student) {
+        peronlist.add(student);
+    }
+
+    void addTeacher(Teacher teacher) {
+        peronlist.add(teacher);
+    }
+
+    void updatePerSon(String id) {
+        Person a=findplayeEveryOne(id);
+        if(a!=null){
+            a.updatePerson(id);
+        }else{
+            System.out.println("Khong tim thay");
+        }
+    }
+
+    void deletePerson(String id) {
+        Person a=findplayeEveryOne(id);
+        if(a!=null){
+            peronlist.remove(a);
+        }else{
+            System.out.println("Khong tim thay");
+        }
+        
+    }
+
+    Person findplayeEveryOne(String id) {
+        
+        for(Person person :peronlist){
+            if(person.getId().equals(id)){
+                return person;
+            }
+        }
+        return null;
+
+    }
+
+    void displayInfor() {
+        for(Person person :peronlist){
+            person.displayInfor();
+        }
+    }
+
+    Student findTopStudent() {
+        Student top=null;
+        for(Person person :peronlist){
+            if(person instanceof Student){
+               Student student = (Student) person;
+               if(top==null||student.getGpa()>top.getGpa()){
+                   top=student;
+               }
+                   
+            }
+        }
+        top.displayInfor();
+        return null;
+        
+
+    }
+
+    Teacher findTeacherByDepartment() {
+        String ByDepartment=sc.nextLine();
+        for(Person person :peronlist){
+            if(person instanceof Teacher){
+                Teacher teacher=(Teacher) person;
+                if(teacher.getDepartment().equals(ByDepartment)){
+                    teacher.displayInfor();
+                }
+            }
+        }
+        return null;
+
+    }
+
+    void checkbookOverdue() {
+
+    }
+}
---
+++ ONTAP/src/ontap/ver5/Ver5.java
@@ -0,0 +1,64 @@
+package ver5;
+
+import java.util.Scanner;
+
+public class Ver5 {
+
+    public static void main(String[] args) {
+        Personlist personlist = new Personlist();
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("1. Add new student\n"
+                    + "2. Add new teacher\n"
+                    + "3. Update person by id\n"
+                    + "4. Delete person by id\n"
+                    + "5. Display all students and teachers\n"
+                    + "6. Find the student with the highest GPA\n"
+                    + "7. Find teachers by department\n"
+                    + "8. Notify whether the book's return due date has arrived or not");
+            int a = sc.nextInt();
+            switch (a) {
+                case 1:
+                    Student student = new Student();
+                    student.addPerson();
+                    personlist.addStudent(student);
+                    break;
+                case 2:
+                    Teacher teacher = new Teacher();
+                    teacher.addPerson();
+                    personlist.addTeacher(teacher);
+                    break;
+                case 3:
+                    sc.nextLine();  
+                    String id = sc.nextLine();
+                    personlist.updatePerSon(id);
+                    
+                    break;
+                case 4:
+                    sc.nextLine(); 
+                    String b = sc.nextLine();
+                    personlist.deletePerson(b);
+                    break;
+                case 5:
+                    sc.nextLine(); 
+                    personlist.displayInfor();
+                    break;
+                case 6:
+                    personlist.findTopStudent();
+                    break;
+                case 7:
+                    personlist.findTeacherByDepartment();
+                    break;
+                case 8:
+
+                    break;
+                case 9:
+
+                    return;
+                default:
+                    System.out.println("");
+            }
+        }
+    }
+
+}
